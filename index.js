7cfd5a863807516fe26c0f71e131c9bc7413ebcd
Kira Fox <foxykira22@gmail.com>
2024-03-06 00:08:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+} 
+
+const btn = document.querySelector('#btn-no');
+
+btn.addEventListener('mouseenter', () => {
+    btn.style.left = `${random(0, 90)}%`;
+    btn.style.top = `${random(0, 90)}%`;
+})
+
